78ac90d40e3f19d6c44e90c02347387125b8d128
Melissa Auclaire <rhapsody.ai@gmail.com>
2021-04-07 20:55:12 -0700

    Add files via upload
    update

--- pypong.py
+++ pypong.py
@@ -16,10 +16,18 @@ def main():
     def draw_centroid():
         pygame.draw.rect(screen, object_color, (SCREEN_WIDTH/2,0, 1, SCREEN_HEIGHT))
 
-    def draw_p1score():
-        pygame.draw.rect(screen, object_color, (SCREEN_WIDTH-(SCREEN_WIDTH/4),0, 1, 100))
+    def draw_score(xpos, ypos, score):
+        if(score == 0):
+            draw0(xpos, ypos)
+        elif(score == 1):
+            draw1(xpos, ypos)
+        elif(score == 2):
+            draw2(xpos, ypos)
+        elif(score == 3):
+            draw3(xpos, ypos)
+        #pygame.draw.rect(screen, object_color, (SCREEN_WIDTH-(SCREEN_WIDTH/4),0, 1, 100))
 
-    def draw_p2score():
+    def draw_p2score(score):
         pygame.draw.rect(screen, object_color, (SCREEN_WIDTH/4,0, 1, 100))
 
     def draw_paddle1():
@@ -31,14 +39,37 @@ def main():
     def random_bool():
         return random.randint(0, 1)
 
-    def draw0():
-        print('zero')
-    def draw1():
-        print('one')
-    def draw2():
-        print('two')
-    def draw3():
-        print('three')
+    def draw0(xpos, ypos):
+        pygame.draw.rect(screen, object_color, (xpos, ypos, 50, 10))
+        pygame.draw.rect(screen, object_color, (xpos, ypos, 10, 50))
+        pygame.draw.rect(screen, object_color, (xpos, ypos+40, 50, 10))
+        pygame.draw.rect(screen, object_color, (xpos+40, ypos, 10, 50))
+    def draw1(xpos, ypos):
+        pygame.draw.rect(screen, object_color, (xpos, ypos, 30, 10))
+        pygame.draw.rect(screen, object_color, (xpos+20, ypos, 10, 50))
+    def draw2(xpos, ypos):
+        pygame.draw.rect(screen, object_color, (xpos, ypos, 50, 10))
+        pygame.draw.rect(screen, object_color, (xpos+40, ypos, 10, 30))
+        pygame.draw.rect(screen, object_color, (xpos, ypos+20, 50, 10))
+        pygame.draw.rect(screen, object_color, (xpos, ypos+20, 10, 30))
+        pygame.draw.rect(screen, object_color, (xpos, ypos+40, 50, 10))
+    def draw3(xpos, ypos):
+        pygame.draw.rect(screen, object_color, (xpos, ypos, 50, 10))
+        pygame.draw.rect(screen, object_color, (xpos+40, ypos, 10, 50))
+        pygame.draw.rect(screen, object_color, (xpos, ypos+20, 50, 10))
+        pygame.draw.rect(screen, object_color, (xpos, ypos+40, 50, 10))
+
+    def reset_ball_dir(ball_dir):
+        if(random_bool() == 1):
+            ball_dir[0] = "right"
+        else:
+            ball_dir[0] = "left"
+        
+        if(random_bool() == 1):
+            ball_dir[1] = "down"
+        else:
+            ball_dir[1] = "up"
+        print(ball_dir)
 
 
     # init pygame
@@ -49,7 +80,8 @@ def main():
     (PADDLE_SIZEX, PADDLE_SIZEY) = (10, 100)
     XDIRECTIONS = ("left", "right")
     YDIRECTIONS = ("up", "down")
-    BALL_SPEED = 2
+    BALL_DEFAULT_POSITION = [(SCREEN_WIDTH/2)-5,(SCREEN_HEIGHT/2)-5]
+    BALL_SPEED = 3
 
     # variables - colors
     background_colour = (0,0,0)  # black background color
@@ -59,7 +91,7 @@ def main():
 
     # variables - misc
     ball_size = (10,10)
-    ball_pos = [(SCREEN_WIDTH/2)-5,(SCREEN_HEIGHT/2)-5]
+    ball_pos = BALL_DEFAULT_POSITION
     paddle1_pos = (100, (SCREEN_HEIGHT/2)-50)
     paddle2_pos = (SCREEN_WIDTH-100, (SCREEN_HEIGHT/2)-50)
 
@@ -67,24 +99,11 @@ def main():
     ball_dir =  ["", ""]
 
     # init ball starting movement direction
-    if(random_bool() == 1):
-        ball_dir[0] = "right"
-    else:
-        ball_dir[0] = "left"
-    
-    if(random_bool() == 1):
-        ball_dir[1] = "down"
-    else:
-        ball_dir[1] = "up"
-
-    print(ball_dir)
+    reset_ball_dir(ball_dir)
 
     # variables - player score (3 to win)
     p1score = 0
     p2score = 0
-
-
-    #window = pygame.display.set_mode((1024, 768))
 
     #create game window
     size = (SCREEN_WIDTH, SCREEN_HEIGHT)
@@ -95,7 +114,6 @@ def main():
     screen.fill(background_colour)
     running = True
 
-    #pygame.draw.rect(screen, object_color,(200,150,100,50))
 
     while running:
         for event in pygame.event.get():
@@ -104,19 +122,15 @@ def main():
 
         # refresh background color
         screen.fill(background_colour)
-  
-
 
         # top and bottom bounds detection
-        if(ball_pos[1] >= 700):
+        if(ball_pos[1] >= (SCREEN_HEIGHT-ball_size[1])):
             if(ball_dir[1] == "down"):
                 ball_dir[1] = "up"
 
         if(ball_pos[1] <= 0):
             if(ball_dir[1] == "up"):
                 ball_dir[1] = "down"
-
-
 
 
         # update ball position
@@ -131,12 +145,35 @@ def main():
             ball_pos[1]-=BALL_SPEED
 
 
+        #check for goal
+        if(ball_pos[0] < 0):
+            p2score+=1
+            ball_pos = [(SCREEN_WIDTH/2)-5,(SCREEN_HEIGHT/2)-5]
+            reset_ball_dir(ball_dir)
+
+            if(p2score > 2):
+                print("P2 is the winner!")
+                exit()
+
+        if(ball_pos[0] > SCREEN_WIDTH):
+            p1score+=1
+            ball_pos = [(SCREEN_WIDTH/2)-5,(SCREEN_HEIGHT/2)-5]
+            reset_ball_dir(ball_dir)
+
+            if(p1score > 2):
+                print("P1 is the winner!")
+                exit()
+
+
         # # draw center line
         draw_centroid()
 
         # draw scores
-        draw_p1score()
-        draw_p2score()
+        draw_score((SCREEN_WIDTH/4)-25, 20, p1score)
+        draw_score((SCREEN_WIDTH-(SCREEN_WIDTH/4))-25, 20, p2score)
+
+        #draw0((SCREEN_WIDTH-(SCREEN_WIDTH/4))-25, 20)
+        #draw3((SCREEN_WIDTH/4)-25, 20)
 
         # draw ball
         draw_rect(ball_pos[0], ball_pos[1], ball_size[0], ball_size[1])
